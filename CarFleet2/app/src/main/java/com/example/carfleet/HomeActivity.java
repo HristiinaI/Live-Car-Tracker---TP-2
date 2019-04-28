9fa99dd403b2284114e60f4737f13cbc7e8a6164
Teddy15 <t.k.yovcheva@gmail.com>
2019-04-28 03:15:59 +0300

    Example Nav Bar for Testing

--- CarFleet2/app/src/main/java/com/example/carfleet/HomeActivity.java
+++ CarFleet2/app/src/main/java/com/example/carfleet/HomeActivity.java
@@ -1,13 +1,56 @@
 package com.example.carfleet;
 
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.Objects;
 
 public class   HomeActivity extends AppCompatActivity {
+
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle abdt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        dl = (DrawerLayout)findViewById(R.id.dl);
+        abdt = new ActionBarDrawerToggle(this, dl, R.string.Open, R.string.Close);
+        abdt.setDrawerIndicatorEnabled(true);
+
+        dl.addDrawerListener(abdt);
+        abdt.syncState();
+
+        android.support.v7.app.ActionBar actionBar =getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        NavigationView nav_view = (NavigationView)findViewById(R.id.nav_view);
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if(id == R.id.my_profile) {
+                    Toast.makeText(HomeActivity.this, "MyProfile", Toast.LENGTH_SHORT).show();
+                }
+                if(id == R.id.create_group) {
+                    Toast.makeText(HomeActivity.this, "CreateGroup", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return abdt.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
     }
 }
